db7bfb8389ef4880b24d5e5c2bc552545562b44b
Adrian Ali <adali@mm-mac-7252.mediamath.local>
2017-06-12 15:18:57 -0400

    1

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
     name='flake8-XPATH',
     description='A flake8 extension that checks for XPATH: statements',
     keywords='flake8 XPATH',
-    version='1.0',
+    version='get_version()',
     author='Adrian Ali',
     author_email='adrianali0730@gmail.com',
     install_requires=['setuptools'],
     entry_points={
         'flake8.extension': [
-            'UIA200 = XPATH:check_XPATH'
+            'UIA-200 = XPATH:check_XPATH'
         ],
     },
     url='https://github.com/adrianali0730/flake8-plugins',
