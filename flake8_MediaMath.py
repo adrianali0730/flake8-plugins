9c2f7aee20151c214965d46481b50d80bda73f98
Adrian Ali <adali@mm-mac-7252.mediamath.local>
2017-06-13 11:43:07 -0400

    statements

--- flake8_MediaMath.py
+++ flake8_MediaMath.py
@@ -5,7 +5,7 @@ import re
 __version__ = '1.0'
 
 CHECKS = [
-    (re.compile(r'\GBy.XPATH'), 'UIA200', 'XPATH statement found. Use CSS Selector instead.'),
+    (re.compile(r'By.XPATH'), 'UIA200', 'XPATH statement found. Use CSS Selector instead.'),
     (re.compile(r'checked'), 'UIA201', 'Remove temporary selection "checked".')
 ]
 
@@ -22,7 +22,7 @@ def statement_usage(logical_line, noqa=None):
     if noqa:
         return
     for regexp, code, message in CHECKS:
-        match = regexp.search(logical_line)
+        match = regexp.finditer(logical_line)
         if match is not None:
             yield match.start(), '{0} {1}'.format(code, message)
             return
