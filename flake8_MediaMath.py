63b3da2b5ab284dd7b65b38fd3bf4a85f3bdc50e
Adrian Ali <adali@mm-mac-7252.mediamath.local>
2017-06-14 15:03:34 -0400

    UIA200 and 201 work.

--- flake8_MediaMath.py
+++ flake8_MediaMath.py
@@ -5,7 +5,7 @@ import re
 __version__ = '1.0'
 
 CHECKS = [
-    (re.compile(r'By.XPATH'), 'UIA200', 'XPATH statement found. Use CSS Selector instead.'),
+    (re.compile(r'\"By.XPATH\"'), 'UIA200', 'XPATH statement found. Use CSS Selector instead.'),
     (re.compile(r'checked'), 'UIA201', 'Remove temporary selection "checked".')
 ]
 
@@ -22,7 +22,7 @@ def statement_usage(physical_line, noqa=None):
     if noqa:
         return
     for regexp, code, message in CHECKS:
-        match = regexp.search(physical_line)
+        match = regexp.search(\"physical_line\")
         if match is not None:
             yield match.start(), '{0} {1}'.format(code, message)
             return
